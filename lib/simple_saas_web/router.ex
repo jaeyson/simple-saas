046b7e19f7ec92c2d2d4bf5fd09ea4d4821edfd7
jaeyson <hello@jaeyson.dev>
2020-02-05 21:34:09 +0700

    added pow_routes() and error handler

--- lib/simple_saas_web/router.ex
+++ lib/simple_saas_web/router.ex
@@ -1,5 +1,6 @@
 defmodule SimpleSaasWeb.Router do
   use SimpleSaasWeb, :router
+  use Pow.Phoenix.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -11,6 +12,22 @@ defmodule SimpleSaasWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :protected do
+    plug Pow.Plug.RequireAuthenticated,
+      error_handler: Pow.Phoenix.PlugErrorHandler
+  end
+
+  scope "/" do
+    pipe_through :browser
+
+    pow_routes()
+  end
+
+  scope "/", SimpleSaasWeb do
+    pipe_through [:browser, :protected]
+
   end
 
   scope "/", SimpleSaasWeb do
